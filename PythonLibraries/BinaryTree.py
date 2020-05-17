b19b9b88418593621d93abe155f2c613f30f7cd1
Mark <m1pearson@me.com>
2020-05-17 04:00:33 +0100

    Add binary tree rendering Finnish section on huffman compression

---
+++ PythonLibraries/BinaryTree.py
@@ -0,0 +1,84 @@
+class BstNode:
+
+    def __init__(self, key, view):
+        self.key = key
+        self.view = view
+        self.right = None
+        self.left = None
+
+    def insert(self, key, view):
+        if self.key == key:
+            return
+        elif self.key < key:
+            if self.right is None:
+                self.right = BstNode(key, view)
+            else:
+                self.right.insert(key, view)
+        else: # self.key > key
+            if self.left is None:
+                self.left = BstNode(key, view)
+            else:
+                self.left.insert(key, view)
+
+    def display_print(self):
+        lines, _, _, _ = self._display_aux()
+        for line in lines:
+            print(line)
+            
+    def get_display_string(self,  location = "binary_tree_pic.txt"):
+        lines, _, _, _ = self._display_aux()
+        the_string = ""
+        for line in lines:
+            the_string += line
+            the_string += "\n"
+        file = open(location, 'w', encoding='utf8')
+        file.write(the_string)
+        file.close()
+        return(the_string)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.view
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.view
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.view
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.view
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+#Based on https://stackoverflow.com/questions/34012886/print-binary-tree-level-by-level-in-python/34014370
+#But I have done my own modification to it
